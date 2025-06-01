710249f2b10a026aff9e94b1cf3a127665997cac
Braxo23 <139518575+Braxo23@users.noreply.github.com>
2025-06-01 01:15:01 +0000

    Building SDK Rich and all

--- chapter7/complete/sdk/src/swcpy/swc_client.py
+++ chapter7/complete/sdk/src/swcpy/swc_client.py
@@ -1,5 +1,5 @@
 import httpx
-import swcpy.swc_config as config
+import chapter7.sdk.swc_config as config
 from .schemas import League, Team, Player, Performance, Counts
 from typing import List
 import backoff
